ad34c55fcfd5ad9a83a42f09a8c00cc071b08e4c
magdapoppins <mmbste@utu.fi>
2017-09-07 14:37:27 +0200

    Javascript WOOHOO!

---
+++ LearnMVC/wwwroot/JavaScript.js
@@ -0,0 +1,18 @@
+﻿
+function DoStuff(id) {
+    document.location.href = "/Quiz/Text/" + id;
+}
+
+//$("#sidebarzItem, #sidebarzItemDone").click(function () {
+//    alert("yey!");
+//    var id = this.CategoryID;
+//    window.location.replace("/Quiz/Text/" + id);
+//})
+
+//$("#open_same_window").click(function () {
+//    //this will find the selected website from the dropdown
+//    var go_to_url = $("#websites").find(":selected").val();
+
+//    //this will redirect us in same window
+//    
+//});

20f094efed86c95f4d6099fce416bc780ca741d3
magdapoppins <mmbste@utu.fi>
2017-09-07 15:54:02 +0200

    More css och sidebar funkar.

--- LearnMVC/Controllers/QuizController.cs
+++ LearnMVC/Controllers/QuizController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using LearnMVC.Models.Entities;
 using Microsoft.AspNetCore.Authorization;
 using LearnMVC.Models.View_Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,10 +16,17 @@ namespace LearnMVC.Controllers
     [Authorize]
     public class QuizController : Controller
     {
+        UserManager<IdentityUser> userManager;
+        IdentityDbContext identityContext;
         QuizDbContext context;
 
-        public QuizController(QuizDbContext context)
+        public QuizController(
+            UserManager<IdentityUser> userManager,
+            IdentityDbContext identityContext,
+            QuizDbContext context)
         {
+            this.userManager = userManager;
+            this.identityContext = identityContext;
             this.context = context;
         }
 
@@ -29,6 +38,7 @@ namespace LearnMVC.Controllers
         public IActionResult Text(int id)
         {
             QuizTextVM qt = context.GetQuizTextVMById(id);
+            qt.SidebarArray = context.GetSidebarVMList(userManager.GetUserId(User));
             return View(qt);
         }
 

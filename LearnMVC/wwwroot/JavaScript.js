acad6a3d1e0c00ce71e8cdc08e6d330f9f0b318d
magdapoppins <mmbste@utu.fi>
2017-09-08 11:14:53 +0200

    Radio buttons

--- LearnMVC/wwwroot/JavaScript.js
+++ LearnMVC/wwwroot/JavaScript.js
@@ -12,31 +12,4 @@ function Plus(id) {
     document.location.href = "/Quiz/Question/" + id;
 }
 
-function SubmitAnswer(id) {
-    document.location.href = "/Quiz/Question/" + id;
-}
 
-
-$(document).ready(function () {
-    $("input[type='button']").click(function () {
-        var radioValue = $("input[name='optradio']:checked").val();
-        if (radioValue) {
-            alert(radioValue);
-        }
-    });
-
-});
-
-//$("#sidebarzItem, #sidebarzItemDone").click(function () {
-//    alert("yey!");
-//    var id = this.CategoryID;
-//    window.location.replace("/Quiz/Text/" + id);
-//})
-
-//$("#open_same_window").click(function () {
-//    //this will find the selected website from the dropdown
-//    var go_to_url = $("#websites").find(":selected").val();
-
-//    //this will redirect us in same window
-//    
-//});

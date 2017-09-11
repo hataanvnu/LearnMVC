8010403a3e9cd932930d2892a52ee296e2e7ed1e
magdapoppins <mmbste@utu.fi>
2017-09-11 13:28:25 +0200

    Admin sidebar

--- LearnMVC/wwwroot/JavaScript.js
+++ LearnMVC/wwwroot/JavaScript.js
@@ -12,4 +12,17 @@ function Plus(id) {
     document.location.href = "/Quiz/Question/" + id;
 }
 
+function GoToResult() {
+    document.location.href = "/Admin/Resultat";
+}
+
+function GoToCreateNew(what) {
+    if (what == 1)
+        document.location.href = "/Admin/AddCategory";
+    else if (what == 2)
+        document.location.href = "/Admin/AddQuizUnit";
+    else if (what == 3)
+        document.location.href = "/Admin/AddQuestion";
+
+}
 

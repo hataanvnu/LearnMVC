591d6aa81e68f017583ce4c1e2d97a389a45bc6e
Johan Bissmark <bissmark.92@gmail.com>
2017-09-11 13:28:12 +0200

    Fixat logik för att lägga till frågor

--- LearnMVC/Models/View Models/AddQuestionVM.cs
+++ LearnMVC/Models/View Models/AddQuestionVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,5 +12,8 @@ namespace LearnMVC.Models.View_Models
 
         public int Order { get; set; }
 
+        public SelectListItem[] PossibleQuizUnits { get; set; }
+
+        public int SelectedQuizUnitId { get; set; }
     }
 }
